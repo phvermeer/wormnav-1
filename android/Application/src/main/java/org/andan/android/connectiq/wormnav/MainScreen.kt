795eb4f42a8c1648302fdcfff283cd39f990aa5b
Peter Vermeer <peter.vermeer@raster.com>
2024-04-20 16:25:12 +0200

    Working basic version

--- android/Application/src/main/java/org/andan/android/connectiq/wormnav/MainScreen.kt
+++ android/Application/src/main/java/org/andan/android/connectiq/wormnav/MainScreen.kt
@@ -1,32 +1,48 @@
 package org.andan.android.connectiq.wormnav
 
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import pt.karambola.gpx.parser.GpxParser
 import kotlin.system.exitProcess
 
 enum class ScreenId(@StringRes val screenId: Int) {
@@ -34,7 +50,6 @@ enum class ScreenId(@StringRes val screenId: Int) {
     SelectTrack(screenId = R.string.select_track),
 }
 
-@ExperimentalMaterial3Api
 @Composable
 fun MainScreen (
     navController: NavHostController = rememberNavController()
@@ -48,13 +63,19 @@ fun MainScreen (
 
     Scaffold(
         topBar = {
-            MainAppBar(
+            MainAppTopBar(
                 currentScreen = currentScreen,
                 canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
+                navigateUp = { navController.navigateUp() },
+            )
+        },
+        bottomBar = {
+            MainAppBottomBar(
+                navController = navController,
             )
         }
-    ) { innerPadding ->
+    ) {
+        innerPadding ->
         NavHost(
             navController = navController,
             startDestination = ScreenId.SelectGpx.name,
@@ -62,7 +83,8 @@ fun MainScreen (
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .padding(innerPadding)
-        ) {
+            ) {
+
             composable(route = ScreenId.SelectGpx.name){
                 SelectGpxScreen(
                     modifier = Modifier
@@ -76,7 +98,7 @@ fun MainScreen (
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainAppBar(
+fun MainAppTopBar(
     currentScreen: ScreenId,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
@@ -111,10 +133,82 @@ fun MainAppBar(
 }
 
 @Composable
+fun MainAppBottomBar(navController: NavHostController){
+    val currentContext = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {uri->
+        uri?.let{setUri(uri, currentContext)}
+    }
+    NavigationBar (
+        content = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 0.dp)
+                ,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                if(navController.previousBackStackEntry != null){
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back_button)
+                        )
+                    }
+                }else{
+                    Spacer(Modifier)
+                }
+                IconButton(
+                    onClick = {
+                        launcher.launch(arrayOf("application/octet-stream"))
+
+                    }
+                ){
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                        contentDescription = stringResource(R.string.next_button)
+                    )
+                }
+            }
+        },
+    )
+}
+
+@Composable
 fun SelectGpxScreen(modifier: Modifier = Modifier){
-    Text("Hoi")
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top
+    ) {
+        for (i in 1..40) {
+            // show current folder
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start),
+            ) {
+                Text(
+                    text = "Body Text",
+                    fontSize = 30.sp
+                )
+            }
+        }
+    }
 }
 
 fun cancel(){
     exitProcess(-1)
+}
+
+fun setUri(uri: Uri, context: Context){
+    try {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val parser = GpxParser()
+        val gpx = parser.parseGpx(inputStream)
+        if(gpx.tracks.size > 0){
+            val track = gpx.tracks[0]
+            SendToDeviceUtility.startDeviceBrowserActivity(context, track)
+        }
+    }catch(e: Exception){
+        Log.e("PeterTest", e.message, e)
+    }
 }
